da6c61d63aae4b34a7614a2d259401aef84eadc3
chiefexb <chief_exb@mail.ru>
2024-09-10 02:22:29 +0300

    123

--- wordpress/backup.sh
+++ wordpress/backup.sh
@@ -6,7 +6,7 @@ fi
 if [  -d $TARGET ]; then
   echo $TARGET exist nothing to do
 fi
-docker run  \
+docker run --network  wordpress_default  \
 --volume $TARGET:/opt \
 mariadb:lts mariadb-dump mydb --user root --host=mydb --password=rootpassword \
 --result-file=/opt/mydb.sql
